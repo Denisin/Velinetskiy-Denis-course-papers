8455b597a619f47d31b5e47289370d0b04688a55
Denisin <104090361+Denisin@users.noreply.github.com>
2025-09-14 12:58:56 +0300

    Update 2_dim.py

--- 2_dim.py
+++ 2_dim.py
@@ -116,34 +116,3 @@ dt = 0.1
 hawkes.track_intensity(dt)
 hawkes.simulate()
 plot_point_process(hawkes)
-
-'''import matplotlib.pyplot as plt
-
-from tick.plot import plot_hawkes_kernels
-from tick.hawkes import SimuHawkesSumExpKernels, SimuHawkesMulti, \
-    HawkesSumExpKern
-
-end_time = 1000
-n_realizations = 10
-
-decays = [.5, 2., 6.]
-baseline = [0.12, 0.07]
-adjacency = [[[0, .1, .4], [.2, 0., .2]],
-             [[0, 0, 0], [.6, .3, 0]]]
-
-hawkes_exp_kernels = SimuHawkesSumExpKernels(
-    adjacency=adjacency, decays=decays, baseline=baseline,
-    end_time=end_time, verbose=False, seed=1039)
-
-hawkes_exp_kernels.end_time = 1000
-hawkes_exp_kernels.simulate()
-
-
-learner = HawkesSumExpKern(decays, penalty='elasticnet',
-                           elastic_net_ratio=0.8)
-learner.fit(hawkes_exp_kernels.timestamps)
-
-fig = plot_hawkes_kernels(learner, hawkes=hawkes_exp_kernels, show=False)
-
-for ax in fig.axes:
-    ax.set_ylim([0., 1.])'''
